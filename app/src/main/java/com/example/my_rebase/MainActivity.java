df291b02570f956caf5b59329dfec1f2134c3215
cpalazis <cpalazis@yahoo.com>
2022-11-21 07:42:51 +0200

    feature 2

--- app/src/main/java/com/example/my_rebase/MainActivity.java
+++ app/src/main/java/com/example/my_rebase/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        System.out.println("feature2");
+
         System.out.println("Εδώ είναι οι νέες αλλαγές");
     }
 }
